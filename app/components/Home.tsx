5c367999bad3f21dd1d5651eb7a34391ea08871b
Ahmed Salem <ahmedsalem292001@gmail.com>
2025-01-06 17:49:23 +0200

    add deliver and our team components

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,13 +1,17 @@
+import Delivey from "./Delivery/Delivery";
 import Feature from "./Features/Feature";
 import HomeSection from "./HomeSection/HomeSection";
 import Burgers from './OurBurgers/Burgers';
+import Team from "./Team/Team";
 
 const Home = () => {
     return (
-    <div>
+    <div className="overflow-hidden">
         <HomeSection/>
         <Feature/>
         <Burgers/>
+        <Delivey/>
+        <Team/>
     </div>)
 }
 export default Home ;
