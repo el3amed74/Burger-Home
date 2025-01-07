441a61780910c7dcf2dddd897143a0b398cc5ce2
Ahmed Salem <ahmedsalem292001@gmail.com>
2025-01-07 02:09:46 +0200

    update eslint and packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,8 @@ const eslintConfig = [
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
+      "no-unused-vars": "off",
+      "react/react-in-jsx-scope": "off"
     },
   }),
 ]
